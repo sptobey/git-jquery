39251298088becb5f313ee21cba337b55c773d35
Samuel Tobey <Samuel.P.Tobey@colorado.edu>
2015-02-22 17:23:45 -0700

    All done but Content-View

--- contents/js/my/repo.js
+++ contents/js/my/repo.js
@@ -3,5 +3,21 @@ function myRepo(full_name){
     // display a particular repo of mine in #details
     // e.g., https://api.github.com/repos/doubleshow/up
 
-    alert('todo')
+    $.get("https://api.github.com/repos/" + full_name, github, function(data) {
+        
+        var repo = data
+        //console.log(repo)
+        
+        $.get("/git-jquery/templates/repoView.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {item: repo})            
+
+            // assign the rendered html to the dom element whose id is #details
+            $("#details").html(html)
+
+        })
+
+    })
+
 }
